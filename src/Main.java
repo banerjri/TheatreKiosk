b3b96fb8415b20bfdedc1443dec9b600713ca5f8
banerjri <banerjri@mail.uc.edu>
2023-06-18 22:10:56 -0500

    project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int Age;
+        System.out.print("Enter your Age : ");
+        if(in.hasNextInt())
+        {
+            Age = in.nextInt();
+            in.nextLine();
+            if(Age > 21)
+            {
+                System.out.print("\nYou get a Wrist Band");
+            }
+        }
     }
 }
